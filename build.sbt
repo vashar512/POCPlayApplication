2f477ed36cc60f6f9774f4464832579b0654f291
Vishal Ashar <vishalashar@Vishals-MBP.home>
2019-05-19 14:54:54 -0400

    Adding more play application configuration

--- build.sbt
+++ build.sbt
@@ -1,7 +1,5 @@
 name := "POCPlayApplication"
-
 version := "0.1"
-
 scalaVersion := "2.12.6"
 
 lazy val root = (project in file(".")).enablePlugins(PlayJava)
--- test/controllers/HomePageControllerTest.java
+++ test/controllers/HomePageControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
